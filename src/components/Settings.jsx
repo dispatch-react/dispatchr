14807c2553eaa3941f20f58d0dbb3eb531752c32
Enzo Matrix <alphanumeric@openmailbox.org>
2016-01-08 18:23:32 +0000

    react-bootstrap Button was needed in settings file

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var Parse = require('parse');
 var ParseReact = require('parse-react');
 
+var Button = require('react-bootstrap').Button;
+
 var Row = require('react-bootstrap').Row;
 var Col = require('react-bootstrap').Col;
 
