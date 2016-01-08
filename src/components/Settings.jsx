b61710747e9b3f6c06c64f59d137ac9ee7da93be
Enzo Matrix <alphanumeric@openmailbox.org>
2016-01-08 16:06:17 +0000

    prepped setting file for development

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,4 +1,9 @@
 var React = require('react');
+var Parse = require('parse');
+var ParseReact = require('parse-react');
+
+var Row = require('react-bootstrap').Row;
+var Col = require('react-bootstrap').Col;
 
 var Settings = React.createClass({
     render: function() {
